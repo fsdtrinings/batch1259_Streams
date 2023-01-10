8038663dd050cf6f65595b8423b694d984b7eaba
Ashish Bansal <87074240+fsdtrinings@users.noreply.github.com>
2023-01-10 14:16:16 +0530

    Added Stream API things
    Note : We use business class as Laptop which is available inside day5 package

--- day10/StreamAPI4.java
+++ day10/StreamAPI4.java
@@ -15,17 +15,18 @@ public class StreamAPI4 {
 		Laptop l2 = new Laptop(102, "HP", 20505, 4);
 		Laptop l3 = new Laptop(103, "Acer", 30000, 8);
 		Laptop l4 = new Laptop(104, "Dell", 150505, 16);
-		Laptop l5 = new Laptop(104, "Dell", 90505, 16);
-		Laptop l6 = new Laptop(104, "Dell", 60505, 16);
-		Laptop l7 = new Laptop(104, "Dell", 55000, 8);
-		Laptop l8 = new Laptop(104, "Dell", 78505, 16);
+		Laptop l5 = new Laptop(105, "Dell", 90505, 16);
+		Laptop l6 = new Laptop(107, "Dell", 60505, 16);
+		Laptop l7 = new Laptop(109, "Dell", 55000,8);
+		Laptop l8 = new Laptop(110, "Dell", 78505, 16);
 		
 		
-		Stream<Laptop> stream = Stream.of(l1,l2,l3,l4,l5); // of method is used to create stream
+		Stream<Laptop> stream = Stream.of(l1,l2,l3,l4,l5,l6,l7,l8); // of method is used to create stream
 		
 		//printAllLaptops(stream);
 		//filterOnLaptops(stream);
-		filterAndSortBasedOnPrice(stream);
+		//filterAndSortBasedOnPrice(stream);
+		filterAndUpdate(stream);
 		
 	}//end main
 	
@@ -44,12 +45,14 @@ public class StreamAPI4 {
 		stream.filter((p)->{
 			if((p.getRamSize() == 8)) return true;
 			else return false; 
-		}).map((lap8Gb)->{
-			 int currentCost = lap8Gb.getCost();
-			 int newCost = currentCost+999;
-			 lap8Gb.setCost(newCost);
-					 
-		})
+		}).map((a)->{
+			int oldPrice = a.getCost();
+			int newPrice = oldPrice + 999;
+			a.setCost(newPrice);
+			return a;
+		}).forEach((lap)->{
+			System.out.println(lap);
+		});
 		
 	}
 	
